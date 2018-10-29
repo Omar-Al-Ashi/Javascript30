d90eaac92e3b7dd4470a404dc98ab0a1784ad621
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-10-29 14:29:09 +0200

    Add comments to JS

--- 03 - CSS Variables/js/main.js
+++ 03 - CSS Variables/js/main.js
@@ -1,8 +1,8 @@
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll("input"); // Get all the inputs
 
 function handleInputChange() {
-    const suffix = this.dataset.sizing || "";
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    const suffix = this.dataset.sizing || ""; // Get the data-sizing property
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); // Setting the properties
 }
 
 inputs.forEach(input => input.addEventListener('mousemove', handleInputChange));
